41ee2949f45c0b698bf5682e450727b3f1d87f9e
Pablo San José <pabloguinness@gmail.com>
2022-08-09 19:25:33 +0200

    Updated script

--- run_interactive.sh
+++ run_interactive.sh
@@ -3,7 +3,7 @@
 if [[ $1 == "build" ]]; then
     docker build -t zephyr_build .
 elif [[ $1 == "root" ]]; then
-    docker run -v /home/sanjo/Programs/docker_zephyr/Projects/:/home/zduser/zephyrproject/zephyr/Projects -itu root zephyr_build:latest
+    docker run -v $(pwd)/Projects/:/home/zduser/zephyrproject/zephyr/Projects -itu root zephyr_build:latest
 else
-    docker run --device=/dev/ttyUSB0 -v /home/sanjo/Programs/docker_zephyr/Projects/:/home/zduser/zephyrproject/zephyr/Projects -it zephyr_build:latest
+    docker run --device=/dev/ttyUSB0 -v $(pwd)/Projects/:/home/zduser/zephyrproject/zephyr/Projects -it zephyr_build:latest
 fi
